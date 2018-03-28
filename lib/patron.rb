f35fc04e9d9bb6c88eb7824d5d5d7b33b0573889
amcolling <amcolling@gmail.com>
2018-03-28 09:39:44 -0600

    progress

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,3 +1,17 @@
-class Patron
 
-end 
+
+class Patron
+  attr_reader :name
+
+  def initialize
+    @name = "Bob"
+    @result = result
+    @interests = []
+
+  end
+
+  def patron_name
+    bob.name
+  end
+
+end
--- test/patron_test.rb
+++ test/patron_test.rb
@@ -5,4 +5,16 @@ require 'minitest/pride'
 
 
  class TestPatron < Minitest::Test
- end
+   def test_if_it_exists
+     patron = Patron.new
+     assert_instance_of Patron, patron
+    end
+
+    def test_patron_names
+      patron = Patron.new
+      result << patron_name("name")
+      assert_equal "Bob", result
+    end
+
+
+  end

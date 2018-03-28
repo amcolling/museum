b52888b3b3e7b82bd355cc18f35c8a934f78042c
amcolling <amcolling@gmail.com>
2018-03-28 10:41:22 -0600

    getting closer but missing something

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,15 +1,29 @@
 
+require 'pry'
+  class Patron
 
-class Patron
-  attr_reader :name,
-              :interests
 
-  def initialize patron_names
-    @name = "Bob"
-    @interests = []
+    attr_reader :name,
+                :interests
+
+
+    def initialize
+      @name = name
+      @interests = []
+    end
+
+    def add_name(name)
+      add_name = @name
+    end
+
+    def add_interests(data)
+      add_interests = @interests.to_s(data)
+    end
+
 
   end
-
-
-
-end
+  # current = @head
+  # current = current.next_node
+  # current.next_node = Node.new
+  # current.to_string
+  # head.next_node.to_string
--- test/patron_test.rb
+++ test/patron_test.rb
@@ -4,14 +4,20 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 
- class TestPatron < Minitest::Test
-  def test_patron_names
-    patron = Patron.new("name")
-    assert_instance_of Patron, patron
+  class TestPatron < Minitest::Test
+    def test_patron_names
+      patron = Patron.new
+      assert_instance_of Patron, patron
     end
-  #
-  # def test_patron_names
-  #   patron = Patron.new("name")
-  #   assert_equal "Bob", patron
-  # end
-end
+
+    def test_add_name
+      patron = Patron.new(name)
+      assert_equal "Bob", patron.add_name
+    end
+
+    def test_add_interests
+      patron = Patron.new(data)
+      assert_equal "Dead Sea Scrolls", patron.add_interests
+    end
+
+  end
